b5ece9a74188e7715829374b4432bffb8857c409
brayanlopez7 <bray7alc@gmail.com>
2021-01-08 13:35:36 -0500

    WS01 Hello World from Brayan López

---
+++ workshops/unit1/WS01/ByeWorld.java
@@ -0,0 +1,12 @@
+// Brayan López
+
+/*
+some lines of comments can
+be written in this way
+thanks
+*/
+class ByeWorld{
+   public static void main(String[] arg){
+     System.out.println("Bye World from Brayan López");
+	}
+}
---
+++ workshops/unit1/WS01/HelloWorld.java
@@ -0,0 +1,12 @@
+// Brayan López
+
+/*
+some lines of comments can
+be written in this way
+thanks
+*/
+class HelloWorld{
+   public static void main(String[] arg){
+     System.out.println("Hello World from Brayan López");
+	}
+}
