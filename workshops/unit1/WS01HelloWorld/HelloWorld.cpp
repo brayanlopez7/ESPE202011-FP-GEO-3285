ff609d510c2360dea4b1941b378ae87eed1cdd36
brayanlopez7 <bray7alc@gmail.com>
2021-01-13 14:52:34 -0500

    assignments

--- workshops/unit1/WS01HelloWorld/HelloWorld.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int grade = 20;
-    //printf() displays information
-	printf("Hello World from \t \" Brayan López! \" =) \n");
-	printf("\n The grade is %d \n", grade); 
-	return 0;
-}
