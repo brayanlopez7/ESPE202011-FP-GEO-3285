612d3e5706bea37899b9dfbb4954b7a2f9d140d9
brayanlopez7 <bray7alc@gmail.com>
2021-01-15 17:12:14 -0500

    WS07Menus

--- workshops/unit1/WS07Menus/WS07Menus/src/ws07menus/WS07Menus.java
+++ workshops/unit1/WS07Menus/WS07Menus/src/ws07menus/WS07Menus.java
@@ -6,6 +6,7 @@
 package ws07menus;
 
 import java.util.Scanner;
+
 /**
  *
  * @author braya
@@ -16,14 +17,14 @@ public class WS07Menus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-Scanner input = new Scanner(System.in);
+
+        Scanner input = new Scanner(System.in);
         boolean mainLoop = true;
         int option;
 
         do {
             System.out.println(" ========= Calculator =======");
-            System.out.println("1. -> Addition");
+            System.out.println("1. -> Adittion");
             System.out.println("2. -> Subtraction");
             System.out.println("3. -> Multiplication");
             System.out.println("4. -> Division");
@@ -43,16 +44,40 @@ Scanner input = new Scanner(System.in);
                     System.out.println("enter addend 2 -> ");
                     addend2 = input.nextInt();
                     sum = addend1 + addend2;
-                    System.out.println(" the sum is --> " + sum);
+                    System.out.println(" the sum is: " + sum);
                     break;
                 case 2:
-                    System.out.println("The subtraction is");
+                    int minuend;
+                    int substrahend;
+                    int difference;
+                    System.out.println("enter minuend -> ");
+                    minuend = input.nextInt();
+                    System.out.println("enter susbtrahend -> ");
+                    substrahend = input.nextInt();
+                    difference = minuend - substrahend;
+                    System.out.println("The subtraction is: " + difference);
                     break;
                 case 3:
-                    System.out.println("The multiplcation is");
+                    int multiplying;
+                    int multiplier;
+                    int product;
+                    System.out.println("enter multiplying -> ");
+                    multiplying = input.nextInt();
+                    System.out.println("enter multiplier -> ");
+                    multiplier = input.nextInt();
+                    product = multiplying * multiplier;
+                    System.out.println("The multiplcation is: " + product);
                     break;
                 case 4:
-                    System.out.println("The division is");
+                    int dividend;
+                    int divisor;
+                    int quotient;
+                    System.out.println("enter dividend -> ");
+                    dividend = input.nextInt();
+                    System.out.println("enter divisor -> ");
+                    divisor = input.nextInt();
+                    quotient = dividend / divisor;
+                    System.out.println("The division is: " + quotient);
                     break;
                 case 5:
                     System.out.println("Good Bye my friend");
